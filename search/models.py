5f68969b0787d208317cdc190c959ca28519af87
Simon <saintexupery@snu.ac.kr>
2017-07-07 14:46:08 +0900

    add search engine type to model

--- search/models.py
+++ search/models.py
@@ -2,5 +2,18 @@ from django.db import models
 
 
 class SearchWord(models.Model):
+    GOOGLE = 'Google'
+    NAVER = 'Naver'
+    DAUM = 'Daum'
+    NAVER_DICT = 'NDict'
+
+    ENGINE_CHOICES = (
+        (GOOGLE, 'Google'),
+        (NAVER, 'Naver'),
+        (DAUM, 'Daum'),
+        (NAVER_DICT, 'Naver_Dict'),
+    )
+
     search_word = models.CharField(max_length=100)
+    engine = models.CharField(max_length=10, choices=ENGINE_CHOICES, default=GOOGLE)
     created_at = models.DateTimeField(auto_now_add=True)
