6b0c717f6c577c3cf5866a85d2a5f386a821d968
Simon <saintexupery@snu.ac.kr>
2017-08-15 15:06:54 +0900

    js example

---
+++ static/js/search_index.js
@@ -0,0 +1,17 @@
+// function return_search_result() {
+//   var search_word = $("input.word").val();
+//   var method = 'GET';
+//   var url = '/api/search/result/';
+//
+//
+//   $.ajax({
+//     method: method,
+//     url: url,
+//     contentType: "application/json",
+//     data: search_word,
+//     dataType: "json",
+//   })
+//     .done(function(data) {
+//       search_word = data
+//     });
+// }
