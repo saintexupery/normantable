ca8cb3eecee33a40a542996f3c40f33efc2a5a57
Simon <saintexupery@snu.ac.kr>
2017-08-15 15:15:49 +0900

    change settings to use postgresql

--- normantable/settings.py
+++ normantable/settings.py
@@ -82,11 +82,13 @@ WSGI_APPLICATION = 'normantable.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+DATABASES = { 'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'erum',
+    'USER': 'erum',
+    'PASSWORD': '1q2w3e4r!',
+    'HOST': '127.0.0.1',
+    'PORT': ''},
 }
 
 
