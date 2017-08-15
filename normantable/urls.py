e411c377f4cbce7fd719c28f5c8b45af3aa3e943
Simon <saintexupery@snu.ac.kr>
2017-08-15 15:07:52 +0900

    static url

--- normantable/urls.py
+++ normantable/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from normantable.views import (
@@ -31,6 +32,8 @@ urlpatterns = [
     # search_urls
     url(r'^search/index/$', search_index, name='search_index'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 if settings.DEBUG:
